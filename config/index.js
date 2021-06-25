1cd00958595d16e3466a98b2eef86aab77bdc7b5
Alfredo Rodriguez <alfredorods@gmail.com>
2021-06-25 15:28:49 -0500

    Revert "ADDED: llave secreta"
    This reverts commit d77f2c17d7fa5f27b23a387c49ced1a9757d118c.

--- config/index.js
+++ config/index.js
@@ -1,5 +1,3 @@
 module.exports = {
-  secret: process.env.NODE_ENV === 'production' ? process.env.SECRET : 'secret',
-  secret2:"esta es mi clave secreta",
-  secret3:"esta es mi clave secreta",
+  secret: process.env.NODE_ENV === 'production' ? process.env.SECRET : 'secret'
 };
