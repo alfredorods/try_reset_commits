b579ba059f93a07ad13f44c4beec78e02b0025d9
Alfredo Rodriguez <alfredorods@gmail.com>
2021-06-25 15:41:05 -0500

    ADDED: enw secrets

--- config/index.js
+++ config/index.js
@@ -1,3 +1,5 @@
 module.exports = {
-  secret: process.env.NODE_ENV === 'production' ? process.env.SECRET : 'secret'
+  secret: process.env.NODE_ENV === 'production' ? process.env.SECRET : 'secret',
+  secret1:"secret-1",
+  "secret2":"secret-2"
 };
